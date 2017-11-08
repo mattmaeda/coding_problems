be7c89756a3b0334880ccdba2d81e921cb02f37b
Matt Maeda <matt@casamaeda.com>
2017-11-08 09:19:21 -0800

    level order traversal

--- binary_tree/btree_functions.py
+++ binary_tree/btree_functions.py
@@ -24,6 +24,23 @@ class BinaryTree(object):
         return 1 + max(self.get_height(root.left), self.get_height(root.right))
 
 
+    def level_order_traversal(self, root):
+        if root is not None:
+            arr = [root]
+            vals = []
+            while len(arr) != 0:
+                node = arr.pop(0)
+                vals.append(node.data)
+
+                if node.left is not None:
+                    arr.append(node.left)
+                if node.right is not None:
+                    arr.append(node.right)
+
+            return vals
+        return None
+
+
 if __name__ == "__main__":
     a1 = [3,5,2,1,4,6,7] # 3
     a2 = [20,50,35,44,9,15,62,11,13] # 4
@@ -34,17 +51,20 @@ if __name__ == "__main__":
     for i in a1:
         root = bt.insert(root, i)
     assert bt.get_height(root) == 3
+    assert bt.level_order_traversal(root) == [3, 2, 5, 1, 4, 6, 7]
 
     bt = BinaryTree()
     root = None
     for i in a2:
         root = bt.insert(root, i)
     assert bt.get_height(root) == 4
+    assert bt.level_order_traversal(root) == [20, 9, 50, 15, 35, 62, 11, 44, 13]
 
     bt = BinaryTree()
     root = None
     for i in a3:
         root = bt.insert(root, i)
     assert bt.get_height(root) == 5
+    bt.level_order_traversal(root) == [25, 12, 39, 9, 19, 31, 55, 23, 35, 41, 60, 70, 90]
 
     print("All tests passed!")
